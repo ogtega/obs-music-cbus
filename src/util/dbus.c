7501ecd6866efb09789fdf365a41e4717cfa3583
Teslim Olunlade <ogtega@users.noreply.github.com>
2022-10-25 14:39:59 +0000

    Getting song information

--- src/util/dbus.c
+++ src/util/dbus.c
@@ -1,5 +1,7 @@
 #include "dbus.h"
 #include <unistd.h>
+#include <util/base.h>
+#include <util/c99defs.h>
 
 GDBusConnection *bus_get()
 {
@@ -25,17 +27,28 @@ GDBusConnection *bus_get()
 
 void signal_cb(GDBusConnection *conn, const gchar *sender_name,
 	       const gchar *object_path, const gchar *interface_name,
-	       const gchar *signal_name, GVariant *parameters, gpointer data)
+	       const gchar *signal_name, GVariant *params, gpointer data)
 {
 	UNUSED_PARAMETER(conn);
 	UNUSED_PARAMETER(sender_name);
 	UNUSED_PARAMETER(object_path);
 	UNUSED_PARAMETER(interface_name);
-	UNUSED_PARAMETER(parameters);
-	UNUSED_PARAMETER(data);
+	UNUSED_PARAMETER(signal_name);
 
-	// struct metadata *meta = data;
-	blog(LOG_INFO, "%s", signal_name);
+	struct metadata *meta = data;
+
+	gchar *source = NULL;
+	g_autoptr(GVariant) dict = NULL;
+	g_autoptr(GVariantIter) arr = NULL;
+
+	g_variant_get(params, "(s@a{sv}as)", &source, &dict, &arr);
+
+	GVariant *metadict = g_variant_lookup_value(dict, "Metadata",
+						    G_VARIANT_TYPE_VARDICT);
+	if (metadict) {
+		g_variant_lookup(metadict, "xesam:title", "s", &meta->title);
+		blog(LOG_INFO, "%s", meta->title);
+	}
 }
 
 guint bus_subscribe(GDBusConnection *conn, struct metadata *data)
